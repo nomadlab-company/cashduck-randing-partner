cbf48b188fdf51a5a3855063d1f242ad7b2cb739
nomadlab-busan <122001657+nomadlab-busan@users.noreply.github.com>
2024-02-27 13:54:35 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// const logoCon = document.querySelector('.logo-container');
+// const widthCheck = document.querySelector('#w-check');
+// const userImg = document.querySelector('.user-img');
+// const titles = document.querySelectorAll('.title');
+
+// logoCon.addEventListener('click', () => {
+//   scrollTo({ top: 0, behavior: 'smooth' });
+// });
